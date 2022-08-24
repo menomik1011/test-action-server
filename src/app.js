9d8e5bc8de064352b1e50fbc401808b4a0231b16
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-08-24 12:18:27 +0900

    add url

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/",(_, res)=>{
+  res.status(200).send("HOME")
+})
+
 app.get("/test", (_req, res) =>  {
   res.status(200).send("Hello world")
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const app =  require( "./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log(`Example app listening on port http://localhost:${port}`),
 );
